0212ef793f222dc92d1b482d0899c8b16de8eef2
Shadan Abdulkarim <shadanabdulkarim94@gmail.com>
2020-05-13 09:51:45 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Code your solutions in this file
+
+const names= ["Ada", "Brendan", "Ali"];
+function writeCards(names,events){
+  let messages=[]
+  for(let i=0;i<names.length;i++)
+  {    
+    messages.push(`Thank you, ${names[i]}, for the wonderful ${events} gift!`)
+    
+    
+  }
+  return messages
+}
+writeCards(names, "birthday");
+
+function countDown(num){
+    let i=num;
+    while(i>=0)
+    {
+     console.log(i--)
+
+    }
+  }
+countDown(10)
+countDown(4)
